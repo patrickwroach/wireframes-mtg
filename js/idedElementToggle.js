0cda7705de768f1b4f78873df42c40c4dbb611fb
roachp3 <roachp3@nationwide.com>
2018-09-21 18:05:13 -0400

    Adding in ffaannnccy shit and stripping out JS

---
+++ js/idedElementToggle.js
@@ -0,0 +1,7 @@
+function idedElementToggle(elementId){  
+      var target = document.getElementById(elementId);       
+      target.classList.toggle("active"); 
+      e.stopPropagation(); 
+
+}
+
